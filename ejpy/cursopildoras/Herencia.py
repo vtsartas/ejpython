030fee1ac3269a5429d6209d6c613ad655c15141
vtsartas <vtsartas@gmail.com>
2019-12-22 13:12:19 +0100

    vídeo 30

--- ejpy/cursopildoras/Herencia.py
+++ ejpy/cursopildoras/Herencia.py
@@ -19,12 +19,57 @@ class Vehiculos():
         self.frena=True
 
     def estado(self):
-        print("Marca: ",self.marca,"\nModelo: ",self.modelo,"\n¿En marcha?: ",self.enmarcha,"\n¿Acelerando?:",self.acelera,"\n¿Frenando?:",self.frena)
+        print("Marca: ",self.marca,"\nModelo: ",self.modelo,"\n¿En marcha?: ",
+        self.enmarcha,"\n¿Acelerando?:",self.acelera,"\n¿Frenando?:",self.frena)
+
+# creamos una clase 'VElectricos'
+class VElectricos():
+    def __init__(self):
+        self.autonomia=100
+
+    def cargarEnergia(self): 
+        self.cargando=True
+
+# la subclase biciElectrica hereda de dos superclases
+class biciElectrica(Vehiculos,VElectricos):
+    pass
+
+# creamos una clase 'Furgoneta' heredera de la clase 'Vehiculo'
+class Furgoneta(Vehiculos):
+    def carga(self,cargar):
+        self.cargado=cargar
+        if (self.cargado):
+            return "La furgoneta está cargada"
+        else:
+            return "La furgoneta no está cargada"
 
 # creamos la subclase 'Moto' a partir de la superclase 'Vehiculos'
 class Moto(Vehiculos):
-    pass
+    hcaballito=""
+    def caballito(self):
+        self.hcaballito="Voy haciendo el caballito"
+    
+    def estado(self):
+        print("Marca: ",self.marca,"\nModelo: ",self.modelo,"\n¿En marcha?: ",
+        self.enmarcha,"\n¿Acelerando?:",self.acelera,"\n¿Frenando?:",self.frena," ",self.hcaballito)
+
 
 miMoto1=Moto("Yamaha","YZR")
 
+miMoto1.caballito()
+
 miMoto1.estado()
+
+print ("\nFurgonetas:")
+miFurgoneta1=Furgoneta("Renault","Kangoo")
+
+miFurgoneta1.arrancar()
+miFurgoneta1.estado()
+print(miFurgoneta1.carga(True))
+
+# BICICLETA ELÉCTRICA
+print ("\nBicicletas:")
+
+miBici=biciElectrica("Orbea","Rayito")
+
+miBici.estado()
